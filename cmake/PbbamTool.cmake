6be3a1b2711c31772f7f79d292e13b76dd8f9d8b
Derek Barnett <dbarnett@pacificbiosciences.com>
2016-01-14 22:19:11 -0800

    bug 27724 - Moved generated version headers out of source tree, into build directory.
    [git-p4: depot-paths = "//depot/software/smrtanalysis/bioinformatics/staging/PostPrimary/pbbam/": change = 170976]

--- cmake/PbbamTool.cmake
+++ cmake/PbbamTool.cmake
@@ -8,9 +8,14 @@ function(create_pbbam_tool)
     cmake_parse_arguments(create_pbbam_tool "" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
 
     # create executable
-    include_directories(${ToolsCommonDir} ${PacBioBAM_INCLUDE_DIRS})
+    include_directories(
+        ${ToolsCommonDir}              # shared tool code
+        ${CMAKE_CURRENT_BINARY_DIR}    # generated version headers
+        ${PacBioBAM_INCLUDE_DIRS}      # pbbam/htslib includes
+    )
     add_executable(${create_pbbam_tool_TARGET} ${create_pbbam_tool_SOURCES})
-    set_target_properties(${create_pbbam_tool_TARGET} PROPERTIES
+    set_target_properties(
+        ${create_pbbam_tool_TARGET} PROPERTIES
         RUNTIME_OUTPUT_DIRECTORY ${PacBioBAM_BinDir}
     )
     target_link_libraries(${create_pbbam_tool_TARGET} pbbam)
